1a3658f105d986af401619f6aa0fd28106454a58
pmalheiros <paulomalheiros@yahoo.com>
2015-05-09 15:08:28 +0300

    edit copyright with github location

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/info/about', function(req, res) {
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log('App listening at http://%s:%s', host, port)
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('App listening at http://%s:%s', host, port);
 })
